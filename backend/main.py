7e2eb9b4d3e893d6e990aab3ed90c0741a22bcf8
sadafaamer-creator <sadaf.aamer@snapdev.ai>
2025-12-24 17:28:10 +0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,7 @@
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent))
+
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
